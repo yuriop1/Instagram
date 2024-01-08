913620cea70041b1e7cf67f9966233ad68ed8951
yuriop1 <yuriop1@naver.com>
2024-01-08 23:52:13 +0900

    instagram

---
+++ instagram/src/__VLS_template.js
@@ -0,0 +1,127 @@
+import { __VLS_internalComponent, __VLS_componentsOption, __VLS_name } from './App.vue';
+
+function __VLS_template() {
+let __VLS_ctx!: InstanceType<__VLS_PickNotAny<typeof __VLS_internalComponent, new () => {}>> & {};
+/* Components */
+let __VLS_otherComponents!: NonNullable<typeof __VLS_internalComponent extends { components: infer C; } ? C : {}> & typeof __VLS_componentsOption;
+let __VLS_own!: __VLS_SelfComponent<typeof __VLS_name, typeof __VLS_internalComponent & (new () => { $slots: typeof __VLS_slots; })>;
+let __VLS_localComponents!: typeof __VLS_otherComponents & Omit<typeof __VLS_own, keyof typeof __VLS_otherComponents>;
+let __VLS_components!: typeof __VLS_localComponents & __VLS_GlobalComponents & typeof __VLS_ctx;
+/* Style Scoped */
+type __VLS_StyleScopedClasses = {};
+let __VLS_styleScopedClasses!: __VLS_StyleScopedClasses | keyof __VLS_StyleScopedClasses | (keyof __VLS_StyleScopedClasses)[];
+/* CSS variable injection */
+/* CSS variable injection end */
+let __VLS_resolvedLocalAndGlobalComponents!: {} &
+__VLS_WithComponent<'Container', typeof __VLS_localComponents, "Container", "Container", "Container">;
+__VLS_intrinsicElements.div; __VLS_intrinsicElements.div; __VLS_intrinsicElements.div; __VLS_intrinsicElements.div;
+__VLS_intrinsicElements.ul; __VLS_intrinsicElements.ul; __VLS_intrinsicElements.ul; __VLS_intrinsicElements.ul; __VLS_intrinsicElements.ul; __VLS_intrinsicElements.ul;
+__VLS_intrinsicElements.li; __VLS_intrinsicElements.li; __VLS_intrinsicElements.li; __VLS_intrinsicElements.li;
+__VLS_intrinsicElements.img;
+__VLS_components.Container;
+// @ts-ignore
+[Container,];
+__VLS_intrinsicElements.input;
+__VLS_intrinsicElements.label; __VLS_intrinsicElements.label;
+{
+const __VLS_0 = __VLS_intrinsicElements["div"];
+const __VLS_1 = __VLS_elementAsFunctionalComponent(__VLS_0);
+const __VLS_2 = __VLS_1({ ...{}, class: ("header"), }, ...__VLS_functionalComponentArgsRest(__VLS_1));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_0, typeof __VLS_2> & Record<string, unknown>) => void)({ ...{}, class: ("header"), });
+const __VLS_3 = __VLS_pickFunctionalComponentCtx(__VLS_0, __VLS_2)!;
+let __VLS_4!: __VLS_NormalizeEmits<typeof __VLS_3.emit>;
+{
+const __VLS_5 = __VLS_intrinsicElements["ul"];
+const __VLS_6 = __VLS_elementAsFunctionalComponent(__VLS_5);
+const __VLS_7 = __VLS_6({ ...{}, class: ("header-button-left"), }, ...__VLS_functionalComponentArgsRest(__VLS_6));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_5, typeof __VLS_7> & Record<string, unknown>) => void)({ ...{}, class: ("header-button-left"), });
+const __VLS_8 = __VLS_pickFunctionalComponentCtx(__VLS_5, __VLS_7)!;
+let __VLS_9!: __VLS_NormalizeEmits<typeof __VLS_8.emit>;
+{
+const __VLS_10 = __VLS_intrinsicElements["li"];
+const __VLS_11 = __VLS_elementAsFunctionalComponent(__VLS_10);
+const __VLS_12 = __VLS_11({ ...{}, }, ...__VLS_functionalComponentArgsRest(__VLS_11));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_10, typeof __VLS_12> & Record<string, unknown>) => void)({ ...{}, });
+const __VLS_13 = __VLS_pickFunctionalComponentCtx(__VLS_10, __VLS_12)!;
+let __VLS_14!: __VLS_NormalizeEmits<typeof __VLS_13.emit>;
+(__VLS_13.slots!).default;
+}
+(__VLS_8.slots!).default;
+}
+{
+const __VLS_15 = __VLS_intrinsicElements["ul"];
+const __VLS_16 = __VLS_elementAsFunctionalComponent(__VLS_15);
+const __VLS_17 = __VLS_16({ ...{}, class: ("header-button-right"), }, ...__VLS_functionalComponentArgsRest(__VLS_16));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_15, typeof __VLS_17> & Record<string, unknown>) => void)({ ...{}, class: ("header-button-right"), });
+const __VLS_18 = __VLS_pickFunctionalComponentCtx(__VLS_15, __VLS_17)!;
+let __VLS_19!: __VLS_NormalizeEmits<typeof __VLS_18.emit>;
+{
+const __VLS_20 = __VLS_intrinsicElements["li"];
+const __VLS_21 = __VLS_elementAsFunctionalComponent(__VLS_20);
+const __VLS_22 = __VLS_21({ ...{}, }, ...__VLS_functionalComponentArgsRest(__VLS_21));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_20, typeof __VLS_22> & Record<string, unknown>) => void)({ ...{}, });
+const __VLS_23 = __VLS_pickFunctionalComponentCtx(__VLS_20, __VLS_22)!;
+let __VLS_24!: __VLS_NormalizeEmits<typeof __VLS_23.emit>;
+(__VLS_23.slots!).default;
+}
+(__VLS_18.slots!).default;
+}
+{
+const __VLS_25 = __VLS_intrinsicElements["img"];
+const __VLS_26 = __VLS_elementAsFunctionalComponent(__VLS_25);
+const __VLS_27 = __VLS_26({ ...{}, src: ("./assets/logo.png"), class: ("logo"), }, ...__VLS_functionalComponentArgsRest(__VLS_26));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_25, typeof __VLS_27> & Record<string, unknown>) => void)({ ...{}, src: ("./assets/logo.png"), class: ("logo"), });
+const __VLS_28 = __VLS_pickFunctionalComponentCtx(__VLS_25, __VLS_27)!;
+let __VLS_29!: __VLS_NormalizeEmits<typeof __VLS_28.emit>;
+}
+(__VLS_3.slots!).default;
+}
+{
+const __VLS_30 = ({} as 'Container' extends keyof typeof __VLS_ctx ? { 'Container': typeof __VLS_ctx.Container; } : typeof __VLS_resolvedLocalAndGlobalComponents).Container;
+const __VLS_31 = __VLS_asFunctionalComponent(__VLS_30, new __VLS_30({ ...{}, }));
+({} as { Container: typeof __VLS_30; }).Container;
+const __VLS_32 = __VLS_31({ ...{}, }, ...__VLS_functionalComponentArgsRest(__VLS_31));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_30, typeof __VLS_32> & Record<string, unknown>) => void)({ ...{}, });
+const __VLS_33 = __VLS_pickFunctionalComponentCtx(__VLS_30, __VLS_32)!;
+let __VLS_34!: __VLS_NormalizeEmits<typeof __VLS_33.emit>;
+}
+{
+const __VLS_35 = __VLS_intrinsicElements["div"];
+const __VLS_36 = __VLS_elementAsFunctionalComponent(__VLS_35);
+const __VLS_37 = __VLS_36({ ...{}, class: ("footer"), }, ...__VLS_functionalComponentArgsRest(__VLS_36));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_35, typeof __VLS_37> & Record<string, unknown>) => void)({ ...{}, class: ("footer"), });
+const __VLS_38 = __VLS_pickFunctionalComponentCtx(__VLS_35, __VLS_37)!;
+let __VLS_39!: __VLS_NormalizeEmits<typeof __VLS_38.emit>;
+{
+const __VLS_40 = __VLS_intrinsicElements["ul"];
+const __VLS_41 = __VLS_elementAsFunctionalComponent(__VLS_40);
+const __VLS_42 = __VLS_41({ ...{}, class: ("footer-button-plus"), }, ...__VLS_functionalComponentArgsRest(__VLS_41));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_40, typeof __VLS_42> & Record<string, unknown>) => void)({ ...{}, class: ("footer-button-plus"), });
+const __VLS_43 = __VLS_pickFunctionalComponentCtx(__VLS_40, __VLS_42)!;
+let __VLS_44!: __VLS_NormalizeEmits<typeof __VLS_43.emit>;
+{
+const __VLS_45 = __VLS_intrinsicElements["input"];
+const __VLS_46 = __VLS_elementAsFunctionalComponent(__VLS_45);
+const __VLS_47 = __VLS_46({ ...{}, type: ("file"), id: ("file"), class: ("inputfile"), }, ...__VLS_functionalComponentArgsRest(__VLS_46));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_45, typeof __VLS_47> & Record<string, unknown>) => void)({ ...{}, type: ("file"), id: ("file"), class: ("inputfile"), });
+const __VLS_48 = __VLS_pickFunctionalComponentCtx(__VLS_45, __VLS_47)!;
+let __VLS_49!: __VLS_NormalizeEmits<typeof __VLS_48.emit>;
+}
+{
+const __VLS_50 = __VLS_intrinsicElements["label"];
+const __VLS_51 = __VLS_elementAsFunctionalComponent(__VLS_50);
+const __VLS_52 = __VLS_51({ ...{}, for: ("file"), class: ("input-plus"), }, ...__VLS_functionalComponentArgsRest(__VLS_51));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_50, typeof __VLS_52> & Record<string, unknown>) => void)({ ...{}, for: ("file"), class: ("input-plus"), });
+const __VLS_53 = __VLS_pickFunctionalComponentCtx(__VLS_50, __VLS_52)!;
+let __VLS_54!: __VLS_NormalizeEmits<typeof __VLS_53.emit>;
+(__VLS_53.slots!).default;
+}
+(__VLS_43.slots!).default;
+}
+(__VLS_38.slots!).default;
+}
+if (typeof __VLS_styleScopedClasses === 'object' && !Array.isArray(__VLS_styleScopedClasses)) {
+}
+var __VLS_slots!: {};
+return __VLS_slots;
+}
---
+++ instagram/src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Post :param="param"/>
+    <Post :param="param"/>
+    <Post :param="param"/>
+</div>
+</template>
+
+<script>
+import Post from './Post.vue';
+export default {
+    name: 'App',
+    components: {
+        Post,
+    },
+    props : {
+        param : Array,
+    },
+}
+</script>
+
+<style>
+
+</style>
