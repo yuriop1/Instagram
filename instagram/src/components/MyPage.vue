fe6050cedce3c51d1931381cb0a4c7e5b9189ce5
yuriop1 <yuriop1@naver.com>
2024-01-16 00:38:47 +0900

    팔로어 표출

--- instagram/src/components/MyPage.vue
+++ instagram/src/components/MyPage.vue
@@ -2,9 +2,9 @@
     <div style="padding : 10px">
       <h4>팔로워</h4>
       <input placeholder="?" />
-      <div class="post-header">
-        <div class="profile" style="background-image:url(follower.image)"></div>
-        <span class="profile-name">{{ follower }}</span>
+      <div class="post-header" v-for="(insta, i) in follower" :key="i">
+        <div class="profile" :style="`background-image:url(${insta.image})`"></div>
+        <span class="profile-name">{{ insta.name }}</span>
       </div>
     </div>
 </template>
